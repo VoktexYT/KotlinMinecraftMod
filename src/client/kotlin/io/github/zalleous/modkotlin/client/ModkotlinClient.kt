97bd69f6b99ef10cb15f05e31002d474ebf801f2
VoktexYT <ubguertin@gmail.com>
2025-04-18 14:40:37 -0400

    Add some protoypes

--- src/client/kotlin/io/github/zalleous/modkotlin/client/ModkotlinClient.kt
+++ src/client/kotlin/io/github/zalleous/modkotlin/client/ModkotlinClient.kt
@@ -1,9 +1,40 @@
 package io.github.zalleous.modkotlin.client
 
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.network.ClientPlayerEntity
+import net.minecraft.text.Text
 
 class ModkotlinClient : ClientModInitializer {
 
+    fun leftClickEvent(callback: (ClientPlayerEntity) -> Unit) {
+        ClientTickEvents.END_CLIENT_TICK.register { client: MinecraftClient ->
+            val player = client.player ?: return@register
+
+            if (client.currentScreen == null && client.options.attackKey.isPressed) {
+                callback(player)
+            }
+        }
+    }
+
+    fun rightClickEvent(callback: (ClientPlayerEntity) -> Unit) {
+        ClientTickEvents.END_CLIENT_TICK.register { client: MinecraftClient ->
+            val player = client.player ?: return@register
+
+            if (client.currentScreen == null && client.options.useKey.isPressed) {
+                callback(player)
+            }
+        }
+    }
+
+    fun sendMessageTo(player: ClientPlayerEntity, text: String) {
+        player.sendMessage(Text.of(text), false)
+    }
+
+
     override fun onInitializeClient() {
+        leftClickEvent { player -> sendMessageTo(player, "LEFT") }
+        rightClickEvent { player -> sendMessageTo(player, "RIGHT") }
     }
 }
--- src/main/kotlin/io/github/zalleous/modkotlin/Modkotlin.kt
+++ src/main/kotlin/io/github/zalleous/modkotlin/Modkotlin.kt
@@ -1,9 +1,69 @@
 package io.github.zalleous.modkotlin
 
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.event.player.AttackBlockCallback
+import net.fabricmc.fabric.api.event.player.UseBlockCallback
+import net.minecraft.entity.effect.StatusEffectInstance
+import net.minecraft.entity.effect.StatusEffects
+import net.minecraft.util.ActionResult
+import net.minecraft.world.World
+import net.minecraft.item.Items
+import net.minecraft.block.Blocks
+import net.minecraft.text.Text
 
 class Modkotlin : ModInitializer {
+    fun prototypeExplosionWoodenAxeWhenAttackBlock() {
+        AttackBlockCallback.EVENT.register { player, world, hand, direct, hitResult ->
+            println(player.getStackInHand(hand))
+
+            if (!world.isClient && player.getStackInHand(hand).item == Items.WOODEN_AXE) {
+                world.createExplosion(player, direct.x.toDouble(), direct.y.toDouble(), direct.z.toDouble(), 4f, World.ExplosionSourceType.TNT)
+            }
+            ActionResult.PASS
+        }
+    }
+
+    fun prototypeDarknessEffectWhenDirtBlockAttackBlock() {
+        AttackBlockCallback.EVENT.register { player, world, hand, direct, hitResult ->
+            if (!world.isClient && player.getStackInHand(hand).item == Items.DIRT) {
+                player.addStatusEffect(StatusEffectInstance(StatusEffects.DARKNESS, 100, 10), player)
+            }
+            ActionResult.PASS
+        }
+    }
+
+    fun prototypeSendMessgeWhenPlayerInteractWithAStick() {
+        UseBlockCallback.EVENT.register { player, world, hand, result ->
+            if (player.getStackInHand(hand).item == Items.STICK) {
+                player.sendMessage(Text.of("Hey stickman!"), false)
+            }
+            ActionResult.PASS
+        }
+    }
+
+    fun prototypeFireStick() {
+        UseBlockCallback.EVENT.register { player, world, hand, hitResult ->
+            val stack = player.getStackInHand(hand)
+
+            if (stack.item == Items.BLAZE_ROD) {
+                val clickedPos = hitResult.blockPos
+                val firePos = clickedPos.up()
+
+                // Check if the space above is air (or replaceable)
+                if (world.getBlockState(firePos).isReplaceable) {
+                    world.setBlockState(firePos, Blocks.FIRE.defaultState)
+                    player.swingHand(hand)
+                }
+            }
+
+            ActionResult.PASS
+        }
+    }
 
     override fun onInitialize() {
+//        prototypeExplosionWoodenAxeWhenAttackBlock()
+//        prototypeDarknessEffectWhenDirtBlockAttackBlock()
+//        prototypeSendMessgeWhenPlayerInteractWithAStick()
+//        prototypeFireStick()
     }
 }
