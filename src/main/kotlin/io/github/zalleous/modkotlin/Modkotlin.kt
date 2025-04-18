7076d433d4f48df40979975e35fe3c2dbbd664b2
VoktexYT <ubguertin@gmail.com>
2025-04-18 15:07:28 -0400

    Add doc

--- src/main/kotlin/io/github/zalleous/modkotlin/Modkotlin.kt
+++ src/main/kotlin/io/github/zalleous/modkotlin/Modkotlin.kt
@@ -12,6 +12,34 @@ import net.minecraft.block.Blocks
 import net.minecraft.text.Text
 
 class Modkotlin : ModInitializer {
+    /**
+     * prototypeExplosionWoodenAxeWhenAttackBlock()
+     *
+     * This prototype is use to make a TNT explosion
+     * when player attack a block with a wooden axe
+     *
+     * AttackBlockEvent:
+     *
+     * (RIGHT CLICK EVENT ATTACK BLOCK)
+     *  AttackBlockCallback.EVENT.register { player, world, hand, direct, hitResult }
+     *      direct    -> block position
+     *      hitResult -> Direction
+     *
+     *  (GET THE PLAYER HAND'S ITEM)
+     *      player.getStackInHand(hand).item
+     *
+     * (CREATE TNT EXPLOSION)
+     *      world.createExplosion(player,
+     *
+     *          (Position X:Y:Z Double type (Not int))
+     *          direct.x.toDouble(), direct.y.toDouble(), direct.z.toDouble(),
+     *
+     *          (Power of the explosion (Float type))
+     *          4f,
+     *
+     *          World.ExplosionSourceType.TNT
+     *      )
+     */
     fun prototypeExplosionWoodenAxeWhenAttackBlock() {
         AttackBlockCallback.EVENT.register { player, world, hand, direct, hitResult ->
             println(player.getStackInHand(hand))
@@ -23,6 +51,16 @@ class Modkotlin : ModInitializer {
         }
     }
 
+    /**
+     * prototypeDarknessEffectWhenDirtBlockAttackBlock()
+     *
+     * This method is used for add player effect
+     * when he attacks a block with the dirt block
+     *
+     * (ADD POTION EFFECT TO PLAYER)
+     * player.addStatusEffect(StatusEffectInstance(StatusEffects.DARKNESS, 100,         10)
+     *                                               ^ Type of effect      ^ duration ^ power
+     */
     fun prototypeDarknessEffectWhenDirtBlockAttackBlock() {
         AttackBlockCallback.EVENT.register { player, world, hand, direct, hitResult ->
             if (!world.isClient && player.getStackInHand(hand).item == Items.DIRT) {
@@ -32,6 +70,20 @@ class Modkotlin : ModInitializer {
         }
     }
 
+    /**
+     * prototypeSendMessgeWhenPlayerInteractWithAStick()
+     *
+     * Send chat message to player when he
+     * left-clicks on a block with a stick
+     *
+     * (LEFT CLICK EVENT)
+     * UseBlockCallback.EVENT.register
+     *      ^ Event when leftClick on block
+     *
+     * (SEND CHAT MESSAGE TO PLAYER)
+     *  player.sendMessage(Text.of("Hey stickman!"), false)
+     *       ^ Send message   ^ text format ^ string   ^ overlay
+     */
     fun prototypeSendMessgeWhenPlayerInteractWithAStick() {
         UseBlockCallback.EVENT.register { player, world, hand, result ->
             if (player.getStackInHand(hand).item == Items.STICK) {
@@ -41,17 +93,39 @@ class Modkotlin : ModInitializer {
         }
     }
 
+    /**
+     * prototypeFireStick()
+     *
+     * This method is used to fire every block than the player want with his blaze stick
+     *
+     * (GET PLAYER HAND ITEM)
+     * val stack = player.getStackInHand(hand)
+     *
+     * (CHANGE BLOCK STATE WITH AN OTHER)
+     * world.setBlockState(firePos, Blocks.FIRE.defaultState)
+     *                     ^ position     ^ Other block
+     *
+     * (PLAY PLAYER ATTACK ANIMATION)
+     * player.swingHand(hand)
+     */
     fun prototypeFireStick() {
         UseBlockCallback.EVENT.register { player, world, hand, hitResult ->
             val stack = player.getStackInHand(hand)
 
+            // If the player item is a blaze rod
             if (stack.item == Items.BLAZE_ROD) {
                 val clickedPos = hitResult.blockPos
+
+                // Get the position of the block above it
                 val firePos = clickedPos.up()
 
                 // Check if the space above is air (or replaceable)
                 if (world.getBlockState(firePos).isReplaceable) {
+
+                    // Change block state
                     world.setBlockState(firePos, Blocks.FIRE.defaultState)
+
+                    // Play attack hand animation
                     player.swingHand(hand)
                 }
             }
